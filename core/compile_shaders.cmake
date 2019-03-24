011258f4146e9307b99b3f4a864d628996a804ba
arcashka <pewpewstol@gmail.com>
2019-03-24 23:12:31 +0700

    fix whitespace

--- core/compile_shaders.cmake
+++ core/compile_shaders.cmake
@@ -3,28 +3,28 @@
 
 function(add_compile_shaders_command ALL_SHADERS_FILE)
 
-    if (NOT WIN32)
-        message(ERROR "NOT IMPLEMENTED")
-        return()
-    endif()
+	if (NOT WIN32)
+		message(ERROR "NOT IMPLEMENTED")
+		return()
+	endif()
 
-    set(SHADERS_DIR "${CMAKE_CURRENT_BINARY_DIR}/shaders")
-    set(COMPILE_SHADERS_FILE "${SHADERS_DIR}/compile_shaders.bat")
-    file(REMOVE_RECURSE "${SHADERS_DIR}")
+	set(SHADERS_DIR "${CMAKE_CURRENT_BINARY_DIR}/shaders")
+	set(COMPILE_SHADERS_FILE "${SHADERS_DIR}/compile_shaders.bat")
+	file(REMOVE_RECURSE "${SHADERS_DIR}")
 
-    file(STRINGS "${ALL_SHADERS_FILE}" ALL_STATIC_SHADERS_LIST)
-    get_filename_component(VULKAN_SDK_DIR ${Vulkan_INCLUDE_DIRS} DIRECTORY)
+	file(STRINGS "${ALL_SHADERS_FILE}" ALL_STATIC_SHADERS_LIST)
+	get_filename_component(VULKAN_SDK_DIR ${Vulkan_INCLUDE_DIRS} DIRECTORY)
 
-    foreach(SHADER ${ALL_STATIC_SHADERS_LIST})
-        file(APPEND ${COMPILE_SHADERS_FILE}
-            "${VULKAN_SDK_DIR}/Bin32/glslangValidator.exe -V ${CMAKE_SOURCE_DIR}/${SHADER}\n"
-        )
-    endforeach()
-    file(APPEND ${COMPILE_SHADERS_FILE} "pause")
+	foreach(SHADER ${ALL_STATIC_SHADERS_LIST})
+		file(APPEND ${COMPILE_SHADERS_FILE}
+			"${VULKAN_SDK_DIR}/Bin32/glslangValidator.exe -V ${CMAKE_SOURCE_DIR}/${SHADER}\n"
+		)
+	endforeach()
+	file(APPEND ${COMPILE_SHADERS_FILE} "pause")
 
-    add_custom_target(compile_shaders
-        COMMAND cmd /c "${COMPILE_SHADERS_FILE}"
-        WORKING_DIRECTORY "${SHADERS_DIR}"
-    )
+	add_custom_target(compile_shaders
+		COMMAND cmd /c "${COMPILE_SHADERS_FILE}"
+		WORKING_DIRECTORY "${SHADERS_DIR}"
+	)
 
 endfunction(add_compile_shaders_command)
